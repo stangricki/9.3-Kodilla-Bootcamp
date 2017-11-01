0f33c6bf5c236691b89aab95dc9f155d2a99ff2c
Krzysztof Safjanowski <krzysztof@safjanowski.pl>
2017-11-01 12:46:05 +0100

    Some tests are always welcome

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 var text = "Velociraptor is a genus of herbivorous ceratopsid dinosaur that first appeared during the late Maastrichtian stage of the late Cretaceous period."
 var dinosaur = "triceratops"
-
 var dinosaurUpperCased = dinosaur.toUpperCase()
-
 var textCharsAfter = text.replace("Velociraptor", dinosaurUpperCased)
 
-console.log(textCharsAfter.slice(0, textCharsAfter.length/2))
+describe('String chain', function () {
+  it('Triceratops is a genius', function () {
+    expect(textCharsAfter.slice(0, textCharsAfter.length / 2)).to.be('TRICERATOPS is a genus of herbivorous ceratopsid dinosaur that first app')
+  })
+});
